3998414905869e4a0af077344812c129767fb088
Ahmed Atef <ahmedatifmahmoud@gmail.com>
2023-03-01 23:29:00 +0200

    second commit

--- controllers/product.js
+++ controllers/product.js
@@ -1,15 +1,20 @@
 const express=require('express')
 const productsModel=require('../models/products')
+const { verifyToken } = require('../middleware/auth')
 
 
 
 exports.getproduct=async(req,res,next)=>{
    try{
+      const token=req.cookies.access_token
+      const name= verifyToken(req).username
+      const admin= verifyToken(req).isAdmin
    let id =req.params.id
   
    const product=await productsModel.findOne({_id:id})
       res.render('product',{
       product: product,
+      name,token,admin,
       title: 'details of product',
     
    })}
